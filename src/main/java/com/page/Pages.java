33d02c851c64b1001eb12dca4f0b21122e882a01
kalaivani152n@gmail.com <kv3364@gmail.com>
2023-08-15 14:29:45 +0530

    vani

---
+++ src/main/java/com/page/Pages.java
@@ -0,0 +1,12 @@
+package com.page;
+
+public class Pages {
+	
+	
+	private void method5() {
+
+		
+		System.out.println("methods");
+	}
+
+}
---
+++ src/test/java/org/Test/Test.java
@@ -0,0 +1,11 @@
+package org.Test;
+
+public class Test {
+
+	
+	private void method() {
+
+		
+		System.out.println("tests");
+	}
+}
